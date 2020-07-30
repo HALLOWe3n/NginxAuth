7706eb59fa1898e22744048d719ff81e76a738d8
Demyan4uk <Romans.Demyanchuk@gmail.com>
2020-07-30 21:09:21 +0300

    fixed

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+import uvicorn
+
+from fastapi import FastAPI, Request
 from starlette.staticfiles import StaticFiles
 
-from .routes.users import router as users_router
+from src.app.routes.users import router as users_router
 
 
 api = FastAPI(title='Authentication Service', version='0.1')
 api.mount("/static", StaticFiles(directory='src/static'), name='static')
 api.include_router(users_router)
+
+
+# @api.middleware('http')
+# def set_headers_from_cookie(request: Request, call_next):
+#     """
+#
+#     :param request:
+#     :return:
+#     """
+#     print(request.headers["Authorization"])
+#     response = call_next(request)
+#     return response
+
+if __name__ == '__main__':
+    uvicorn.run(api, host='127.0.0.1', port=8000)
--- src/app/routes/users.py
+++ src/app/routes/users.py
@@ -1,4 +1,5 @@
 import typing
+from pprint import pprint
 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
@@ -38,27 +39,25 @@ def login_template(request: Request):
 def login_user(
         username: str = Form('username'),
         password: str = Form('password'),
-        redirect_uri: str = Form('redirect_uri'),
+        # redirect_uri: str = Form('redirect_uri'),
 ):
-    if redirect_uri == 'None':
-        raise HTTPException(status_code=400, detail='None have redirect uri')
+    # if redirect_uri == 'None':
+    #     raise HTTPException(status_code=400, detail='None have redirect uri')
     auth = Auth()
     user_payload = auth.check_user(username=username, password=password)
     bearer_tokens = auth.create_tokens(payload=user_payload)
+    # response = RedirectResponse(
+    #     redirect_uri,
+    #     status_code=HTTP_303_SEE_OTHER,
+    # )
+    # response.set_cookie(key='access_token', value=bearer_tokens['access_token'])
+    # response.set_cookie(key='refresh_token', value=bearer_tokens['refresh_token'])
 
-    response = RedirectResponse(
-        redirect_uri,
-        status_code=HTTP_303_SEE_OTHER,
-    )
-
-    response.set_cookie(key='access_token', value=bearer_tokens['access_token'])
-    response.set_cookie(key='refresh_token', value=bearer_tokens['refresh_token'])
-
-    return response
+    return bearer_tokens
 
 
 @router.get('/example')
-def example_redirect(user: User = Depends(Auth().get_user)):
+def example_redirect():
     return {'message': 'success'}
 
 
@@ -67,6 +66,9 @@ def new_example_redirect(user: User = Depends(Auth().get_user)):
     return {'message': 'success'}
 
 
-@router.get('/auth')
+@router.get('/auth', name='auth')
 def check_auth_user(user: User = Depends(Auth().get_user)) -> typing.Dict:
     return {'Authorization': True}
+
+
+# Cookie: _xsrf=2|b4657542|9406c8a78a5099e10b96bf69e8d5d1ad|1595599855; authorization="Bearer eyJKHOHjkshdgfmngd.kdjhd.dfgfdsg"; s="Bearer eyJKHOHjkshdgfmngd.kdjhd.dfgfdsg"; username-localhost-8888="2|1:0|10:1595940376|23:username-localhost-8888|44:YWFhYTRmN2FhNmVmNGY5YTg5ZDM0ODU3MDczZWY0OTc=|06ad42ca95539c0cb6b8e5cd32a9335296ca13540c09917ef08bedeb57a217e2"; access_token=
